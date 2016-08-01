d2b9f684e0e2807ad9b14201cc583de3a4e2c069
moshen ahmed bukenya <moshenahmed@gmail.com>
2016-08-01 19:54:35 +0200

    morepractice

--- Multiplesof3and5.java
+++ Multiplesof3and5.java
@@ -2,16 +2,23 @@
 public class Multiplesof3and5 {
 
 	public static void main(String[] args) {
-		
+		int sumone = 0;
+		int sumtwo = 0;
+		int total;
 		for (int i = 0; i < 1000; i++){
 			if (i%3 == 0){
-				System.out.println(i);
+				sumone = sumone + i;
+				// System.out.println(sumone);
 			}
 			else if (i%5 == 0){
-				System.out.println(i);
+				sumtwo = sumtwo + i;
+				//System.out.println(sumtwo);
 			}
+			
+			
 		}
-		
+		total = sumone + sumtwo;
+		System.out.println(" the total is :" + total);
 
 	}
 
