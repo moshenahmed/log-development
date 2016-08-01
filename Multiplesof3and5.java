efd8b16f66a03fbe4ce6e5def6263eabe7e419a8
moshen ahmed bukenya <moshenahmed@gmail.com>
2016-08-01 13:46:40 +0200

    more today

---
+++ Multiplesof3and5.java
@@ -0,0 +1,18 @@
+
+public class Multiplesof3and5 {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 1000; i++){
+			if (i%3 == 0){
+				System.out.println(i);
+			}
+			else if (i%5 == 0){
+				System.out.println(i);
+			}
+		}
+		
+
+	}
+
+}
